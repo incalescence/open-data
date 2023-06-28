8f593f077b8b459091e1d4f09a2de2b8fa7a7ac9
wanning <z5371162@unsw.edu.au>
2023-06-28 16:42:24 +1000

    able to srape into csv format

--- scraping/tiktok.py
+++ scraping/tiktok.py
@@ -3,8 +3,11 @@ This file scrapes from Tiktok using the TikTokApi library
 Which can be accessed at https://github.com/davidteather/TikTok-Api
 The tool requires cookies of a registered user which I've put into cookies.json
 We will be extracting information from the trending page of the app
+
 '''
 from TikTokApi import TikTokApi
+from flatten_json import flatten
+import pandas as pd
 import json
 
 def get_cookies_from_file():
@@ -22,11 +25,16 @@ cookies = get_cookies_from_file()
 def get_cookies(**kwargs):
     return cookies
 
-with open('tiktok-data/data.json', 'w') as f:
-    with TikTokApi() as api:
-        api._get_cookies = get_cookies
-        for trending_video in api.trending.videos():
-            f.write(json.dumps(trending_video.as_dict))
-            f.write('\n')
+videos = []
+with TikTokApi() as api:
+    api._get_cookies = get_cookies
+    for trending_video in api.trending.videos(2):
+        video_data = flatten(trending_video.as_dict)
 
-f.close()
+        for key in list(video_data.keys()):
+            if 'challenge' in key or 'duet' in key or 'sticker' in key or 'avatar' in key or 'url' in key.lower() or 'cover' in key.lower() or 'textExtra' in key or 'Addr' in key or 'subtitleInfos' in key:
+                del video_data[key]
+        videos.append(video_data)
+
+    df = pd.DataFrame.from_dict(videos)
+    df.to_csv('tiktok-data/tiktok.csv', index=False)
